1e3b7eb6381a9ab583bfe9531dd5cf8c48b50244
5tarlight <developerstarlight@gmail.com>
2019-06-19 22:38:35 +0900

    props

--- src/routes/PugRouter.ts
+++ src/routes/PugRouter.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from "express";
 import Logger from "../logger";
 
+export interface RouterProps {
+  url: string
+  type: string
+}
+
 class PugRouter {
   join(req: Request, res: Response) {
     Logger.warn('Router main method is not overrided')
+  }
+
+  props: RouterProps = {
+    url: '/',
+    type: 'get'
   }
 }
 
