1fe74a597fd657d55179a5e6004292fd3cc853ff
5tarlight <developerstarlight@gmail.com>
2019-06-30 11:07:48 +0900

    close window

--- src/public/js/global.js
+++ src/public/js/global.js
@@ -3,8 +3,10 @@ const filter = "win16|win32|win64|mac";
 
 if ((navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1)) {
   alert("이 사이트는 Internet Explorer 를 지원하지 않습니다.");
+  window.close()
 }
 
 if (!(navigator.platform && filter.indexOf(navigator.platform.toLowerCase()) >= 0)) {
   alert("이 사이트는 모바일 접근을 지원하지 않습니다.");
+  window.close()
 }
