539728f6347bc682083c42fb3af4f51be91b8acc
5tarlight <developerstarlight@gmail.com>
2019-07-06 18:19:30 +0900

    login btn css

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
 export { default as Portal } from './page/Portal'
 export { default as Game } from './page/Game'
 export { default as Login } from './page/Login'
+export { default as SingUp } from './page/SignUp'
